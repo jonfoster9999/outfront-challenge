a5bcef4a5737217506c9fbfd817983842b89116a
Jon foster <redcabinny@gmail.com>
2017-08-24 23:55:28 -0400

    changed server

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'http') {
+      return res.redirect(
+       ['http://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
